8b90faafb2c389afd24c159dbb3572c5922e2201
Gjergji <makegjergji@gmail.com>
2016-10-20 11:42:47 +0200

    Please do not change the design of this page Justine , now it looks perfect

--- views/programm/programm.php
+++ views/programm/programm.php
@@ -11,7 +11,7 @@ $tourArray=$_SESSION['tour'];
 
 		<title>Programm overview</title>
 
-		<link rel="stylesheet" type="text/css" href="http://localhost/cas_montana/public/css/main.css">
+		<link rel="stylesheet" type="text/css" href="/<?php echo SITE_NAME; ?>/public/css/main.css">
 
 		<script>
 		   $(function() {
@@ -133,150 +133,165 @@ $tourArray=$_SESSION['tour'];
 		</style>
 	</head>
 	<body>
-<<<<<<< HEAD
 		<div class="wrapper" style= "padding-bottom: 2%; padding-right: 2%; padding-left: 2%;">
 			<h1>Hikinglists</h1>
-			<div id="topButton">
-				<a href="<?php echo URL_DIR.'search/search_path.php'?>"><img id="search1" src="<?php echo URL_DIR. 'public/img/searchRoute.png'?>"></a>
-=======
-		<div style="height: 110%;">
-			<h1>Hikinglists</h1>
-			<div id="topButton">
-				<a  href = "<?php echo URL_DIR.'search/search_path'?>">
-				<button id="searchButton"><img id="search1" src="http://localhost/cas_montana/public/img/search.png" >Search route</button>
-			</a>
->>>>>>> origin/master
-			</div>
+				<div id="topButton">
+					<a href="<?php echo URL_DIR.'search/search_path.php'?>"><img id="search1" src="<?php echo URL_DIR. 'public/img/searchRoute.png'?>"></a>
+				</div>
+			
+			<div>
+			
 			<div id="top">
 			<table style="width: 100%;">
-			<tr>
-			<td style="text-align: center; width: 50%;">
-				<h2>Single hikes</h2>
-			</td>
-			<td style="text-align: center; width: 50%;">
-				<h2>Multi-day hikes</h2>
-			</td>
-			</tr>
+				<tr>
+					<td style="text-align: center; width: 50%;">
+						<h2>Single hikes</h2>
+					</td>
+					<td style="text-align: center; width: 50%;">
+						<h2>Multi-day hikes</h2>
+					</td>
+				</tr>
+			</table>
+			</div>
+			
+			<div id="tableDiv">
+
+				<table id = "mainTable">
+					<tr>
+						<td style="width: 50%;">
+							<div id="insideTable">
+
+							<table>
+								<tr>
+				  					<td>Choose a day:</td>
+				  					<td><input type="text" id="datepicker" name="from"></td>
+								</tr>
+								<tr>
+									<td>Route for the selected day:</td>
+									<td>...</td>
+								</tr>
+								<tr>
+									<td></td>
+					
+								</tr>
+			  				</table>
+							</div>
+						</td>
+
+						<td style="width: 50%;">
+							<div id="insideTable">
+							
+			  				<table>
+			  					<tr>
+			  						<td>Choose date</td>
+			  					</tr>
+			  					<tr>
+				    				<td>
+				    					<b>From:</b>
+				    				</td>
+				    				<td>
+				    					<input type="text" id="datepicker" name="from">
+				    				</td>
+								</tr>
+								<tr>
+									<td>
+										<b>To:</b>
+									</td>
+									<td>
+										<input type="text" id="datepicker" name="to">
+									</td>
+								</tr>
+								<tr>
+									<td>Route for the selected stay:</td>
+									<td>....</td>
+								</tr>
+								<tr>
+									<td></td>
+					
+								</tr>
+			  				</table>
+
+							</div>
+						</td>
+
+					</tr>
 			</table>
 		</div>
-<div id="tableDiv">
 
-<table id = "mainTable">
-		<tr>
-			<td style="width: 50%;">
-				<div id="insideTable">
-
-				<table>
-				<tr>
-				  <td>Choose a day:</td>
-				  <td><input type="text" id="datepicker" name="from"></td>
-				</tr>
-				<tr>
-					<td>Route for the selected day:</td>
-					<td>...</td>
-				</tr>
-				<tr>
-					<td></td>
-					
-				</tr>
-			  	</table>
-				</div>
-			</td>
-
-			<td style="width: 50%;">
-
-				<div id="insideTable">
-			  	<table>
-			  	<tr>
-			  		<td>Choose date</td>
-			  	</tr>
-			  	<tr>
-				    <td><b>From:</b></td>
-				    <td><input type="text" id="datepicker" name="from"></td>
-				</tr>
-				<tr>
-					<td><b>To:</b></td>
-					<td><input type="text" id="datepicker" name="to"></td>
-				</tr>
-				<tr>
-					<td>Route for the selected stay:</td>
-					<td>....</td>
-				</tr>
-				<tr>
-					<td></td>
-					
-				</tr>
-			  	</table>
-
-				</div>
-			</td>
-
-		</tr>
-	</table>
-</div>
-
-<div id="buttonsDiv1">
+		<div id="buttonsDiv1">
 			<table style="width: 100%;">
-			<tr>
-			<td>
-				<form action="<?php echo URL_DIR.'programm/programm'?>" method= "post">
-					<button id="submit1" name ="type" value = '1'>Only hike</button>
-				</from>
-			</td>
-			<td>
-				<form action="<?php echo URL_DIR.'programm/programm'?>" method= "post">
-					<button id="submit1" name ="type" value = '2'>Only multi-hike</button>
-				</form>
-			</td>
-			</tr>
+					<tr>
+						<td>
+							<form action="<?php echo URL_DIR.'programm/programm'?>" method= "post">
+								<button id="submit1" name ="type" value = '1'>Only hike</button>
+							</form>
+						</td>
+						<td>
+							<form action="<?php echo URL_DIR.'programm/programm'?>" method= "post">
+								<button id="submit1" name ="type" value = '2'>Only multi-hike</button>
+							</form>
+						</td>
+					</tr>
 			</table>
 		</div>
 		
-<div id="yourProg">
+		<div id="yourProg">
 			<h2>Your Programm:</h2>
-			</div>
-			<div id="bottom">
-					<table id="table">
-
+		</div>
+		<div id="bottom">
+			<table id="table">
 					 <tr>
               			<th> Type </th>
 						 <th> Titre </th>
 						 <th> Information </th>
 						 <th> Depart</th>
 						 <th> Date debut </th>
-
-
 					 </tr>
-
-					<form action="<?php echo URL_DIR.'programm/hiking_detail'?>" method= "post">
-					 <?php foreach( $tourArray as $cle => $element)
-
-					 {?>
 					 <tr>
-						 	<button  name ="selectedTour" value = <?php echo $cle?>>
-	            <p><?php if( $tourArray[$cle]->getIdxTypeTour() == 1){
-                echo "Hiking";
-              }
-              elseif ($tourArray[$cle]->getIdxTypeTour() == 2) {
-                echo "Stay";
-              }
-              else {
-                echo "Proposal";
-              } ?> </p>
+					  <td colspan = "5">
+						<form action="<?php echo URL_DIR.'programm/hiking_detail'?>" method= "post">
+					 		<?php foreach( $tourArray as $cle => $element)
 
-							<p><?php echo ' '.$tourArray[$cle]->getTitre(); ?></p>
-							<p><?php echo ' '.$tourArray[$cle]->getInformation_fr(); ?></p>
-
-
-				</button>
-
-					<?php } ?>
-			</form>
+					 		{?>
+					 	<button  name ="selectedTour" value = <?php echo $cle?>>
+					 		<table style="width: 100%;">
+					 			<tr>
+					 			<td style="width: 25%">
+	            				<?php if( $tourArray[$cle]->getIdxTypeTour() == 1)
+	            					{
+                					echo "Hiking";
+	            					}
+             							 elseif ($tourArray[$cle]->getIdxTypeTour() == 2) 
+             							 {
+                							echo "Stay";
+             							 }
+              							else 
+              								{
+                							echo "Proposal";
+              								}
+              					?> 
+              					</td>
+					 			<td style="width: 25%">
+              					<?php echo ' '.$tourArray[$cle]->getTitre(); ?>
+								</td>
+					 			<td style="width: 25%">
+								<?php echo ' '.$tourArray[$cle]->getInformation_fr(); ?>
+								</td>
+					 			<td style="width: 25%">
+								<?php echo ' '.$tourArray[$cle]->getDateDebut() ?>
+								</td>
+								</tr>
+							</table>
+						</button>
+							<?php } ?>
+						</form>
+						</td>
 					</tr>
 			</table>
 
 			</div>
-			</div>
+		</div>
+		</div>
 	</body>
 </html>
 
