1ceeb91ae67f6b9c581a28e85c653bf8c60f8e51
JMauris <justine.mauris@students.hevs.ch>
2016-10-27 15:10:39 +0200

    add Language

---
+++ languages/lang.de.php
@@ -0,0 +1,202 @@
+<?php
+/*
+------------------
+Language: German
+------------------
+*/
+ 
+$lang = array();
+
+/*
+------------------
+For all pages
+------------------
+*/
+$lang['BTN_SEARCH'] = 'Route suchen';
+$lang['MY_FAVORITES'] = 'Meine Favoriten';
+$lang['DATE_BEGIN'] = 'Anfangsdatum:';
+$lang['PROGRAMM_DETAIL'] = 'Programmdetails';
+$lang['REGISTER'] = 'registrieren';
+
+/*
+ ------------------
+ search path pages
+ ------------------
+ */
+$lang['SEARCH'] = 'Suchen';
+$lang['REGION'] = 'Region: ';
+//DIFFICULTY FROM ANOTHER PAGE
+$lang['SORT_HIKE'] = 'Wanderungssorte:';
+$lang['HIKINGS'] = 'Wanderungen';
+$lang['PLACEHOLDER'] = 'Filter die Resultate deiner Suche hier, gib den Namen der Stadt ein.';
+
+$lang['ALL'] = 'Alle';
+$lang['SNOWSHOE'] = 'Schneeschuhlaufen';
+$lang['SKI_TOUR'] = 'Skitour';
+$lang['OTHERS'] = 'Andere';
+$lang['WINTER_HIKES'] = 'Winterwanderungen im Schnee';
+$lang['ALP_HIKES'] = 'Alpine Wanderungen';
+$lang['MULTI_D_HIKE'] = 'mehrtätige Wanderungen';
+
+
+$lang['AVAIL_HIKINGS'] = 'Verfügbare Wanderungen';
+$lang['AVAIL_LIST'] = 'Dies sind die verfügbaren Wanderungen für den gewählten Tag:';
+
+/*
+ ------------------
+ Home page
+ ------------------
+ */
+$lang['HOME_TITLE'] = 'Willkommen bei CAS Montana';
+$lang['HOME_TEXT_1'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+$lang['HOME_TEXT_2'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+$lang['HOME_TEXT_3'] = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+
+/*
+ ------------------
+ Program page
+ ------------------
+ */
+$lang['PROGRAM_TITLE'] = 'Liste der Wanderungen';
+
+$lang['SINGLE_HIKES'] = 'Einzelne Wanderungen';
+$lang['CHOOSE_A_DAY'] = 'Wählen Sie einen Tag:';
+$lang['DAY_ROUTE'] = 'Route für den gewählten Tag:';
+$lang['BTN_ONLY_HIKE'] = 'Einzelwanderung';
+
+$lang['MULTI_DAY_HIKES'] = 'Mehrtägige Wanderungen';
+$lang['CHOOSE_DATE'] = 'Datum wählen';
+$lang['FROM'] = 'Von:';
+$lang['TO'] = 'Bis:';
+$lang['STAY_ROUTE'] = 'Route für den gewählten Aufenthalt:';
+$lang['BTN_ONLY_MULTI'] = 'Mehrtageswanderung';
+
+$lang['YOUR_PROGRAMM'] = 'Dein Programm:';
+$lang['BTN_VIEW'] = 'alle anzeigen';
+
+/*
+ ------------------
+ My Program page
+ ------------------
+ */
+$lang['MY_PROGRAM_TITLE'] = 'Meine Programmübersicht';
+$lang['MY_REGISTRATIONS'] = 'Meine Registrationen';
+
+/*
+ ------------------
+ Proposal page
+ ------------------
+ */
+$lang['PROPOSAL_TITLE'] = 'Vorschläge';
+$lang['PROPOSAL_SUBTITLE'] = 'Dies sind unsere Vorschläge:';
+$lang['BTN_NEAR'] = 'in der Nähe';
+
+/*
+ ------------------
+ Proposal_detail page
+ ------------------
+ */
+$lang['PROPOSAL_DETAIL_TITLE'] = 'Detailierte Vorschläge';
+$lang['PLACE_OF_DEP'] = 'Ort der Abreise: ';
+$lang['DESTINATION'] = 'Ziel: ';
+$lang['DIFFICULTY'] = 'Schwierigkeitsgrad: ';
+$lang['DURATION'] = 'Dauer: ';
+$lang['DESCRIPTION'] = 'Beschreibung: ';
+$lang['POPULARITY'] = 'Beliebtsheit bei den SAC Mitgliedern: ';
+$lang['OPINION'] = 'Meine Meinung teilen: ';
+$lang['BTN_BACK'] = 'zurück';
+
+/*
+ ------------------
+ SearchProposal page
+ ------------------
+ */
+$lang['SEARCH_PROPOSAL_TITLE'] = 'Vorschläge Suchen';
+
+/*
+ ------------------
+ My Proposal page
+ ------------------
+ */
+$lang['MY_PROPOSAL_TITLE'] = 'Meine Vorschläge';
+$lang['MY_OPINIONS'] = 'Meine Meinungen';
+
+/*
+ ------------------
+ Contact page
+ ------------------
+ */
+$lang['CONTACT_TITLE'] = 'Kontaktinformationen';
+
+/*
+ ------------------
+ About page
+ ------------------
+ */
+$lang['ABOUT_TITLE'] = 'Informationen über SAC';
+
+$lang['NAME'] = 'Name:';
+$lang['ESTABLISHMENT'] = 'Einrichtung:';
+$lang['HEADQUARTERS'] = 'Hauptsitz:';
+$lang['MAINORGANISATION'] = 'Hauptorganisation:';
+$lang['CERTIFICATION'] = 'Zertifizierung:';
+$lang['FIELD_OF_ACTIVITY'] = 'Tätigkeitsbereich:';
+$lang['AMOUNT_OF_MEMBERS'] = 'Anzahl der Mitglieder:';
+$lang['MEMBERSHIP_FEE'] = 'Mitgliedsbeitrag:';
+$lang['ADVANTAGES'] = 'Vorteile für Mitglieder:';
+$lang['STATUTES'] = 'Statuten:';
+
+//DESCRIPTIONS
+$lang['DES_NAME'] = 'SAC ist die Abkürzung für Schweizer Alpen Club';
+$lang['DES_FIELD_OF_ACTIVITY'] = 'Organisation mit mehr als 50 Wanderungen, Wanderwochenenden und -wochen, Winterwanderungen und Schneeschuhlaufen';
+
+$lang['DES_MEMBERSHIP_FEE_1'] = 'einzelen Personen und Familien - CHF 50.-';
+$lang['DES_MEMBERSHIP_FEE_2'] = 'Gemeinden und Tourismusverbände - CHF 100.-';
+$lang['DES_MEMBERSHIP_FEE_3'] = 'Mitgliedschafts Anmeldeformular';
+
+$lang['DES_ADVANTAGES_1'] = 'Die Teilnahme an den Wanderungen ohne Aufpreis (CHF 5.-)';
+$lang['DES_ADVANTAGES_2'] = 'Preisnachlass auf alle Karten des Bundesamtes für Landestopografie';
+$lang['DES_ADVANTAGES_3'] = 'Sie sind berechtigt an den mehrtätigen Wanderungen und Wanderwochen teilzunehmen';
+$lang['DES_ADVANTAGES_4'] = '1 Wanderindex mit jährlich mehr als 50 neue Wandertipps';
+$lang['DES_ADVANTAGES_5'] = '6 Mal im Jahr erhalten Sie die Zeitschrift - Wanderland Schweiz';
+
+$lang['DES_STATUTES'] = 'Statuten als pdf herunterladen';
+
+/*
+ ------------------
+ My Profil page
+ ------------------
+ */
+$lang['PROFIL_TITLE'] = 'Mein Profil';
+$lang['FIELD_OBLIGATORY'] = '* = obligatorische Felder';
+
+$lang['P_NAME'] = 'Vorname* :';
+$lang['P_LASTNAME'] = 'Nachname* :';
+$lang['P_ADRESS'] = 'Adresse* :';
+$lang['P_NPA'] = 'NPA* :';
+$lang['P_LOCALITY'] = 'Localität* :';
+$lang['P_PHONE'] = 'Telefon :';
+$lang['P_MOBILE'] = 'Handy :';
+$lang['P_EMAIL'] = 'E-mail :';
+$lang['P_ABO'] = 'Abonnement für öffentliche Verkehrsmittel :';
+
+$lang['BTN_SAVE'] = 'Speichern';
+$lang['LINK_CHANGE_PW'] = 'Passwort ändern';
+
+$lang['SELECTION_1'] = 'Bitte wählen';
+$lang['SELECTION_2'] = 'nichts';
+$lang['SELECTION_3'] = 'GA';
+$lang['SELECTION_4'] = 'halb tax';
+
+
+/*
+ ------------------
+ login page
+ ------------------
+ */
+$lang['MON_CAS'] = 'Mein CAS';
+$lang['LOGIN_EMAIL'] = 'E-mail: ';
+$lang['LOGIN_PW'] = 'Passwort: ';
+$lang['WELCOME_TITLE'] = 'Willkommen ';
+
+?>
