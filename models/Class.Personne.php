0fbcf816809b4271ec3f53135a8422f70d403e03
JMauris <justine.mauris@students.hevs.ch>
2016-11-06 16:18:38 +0100

    Revert change on Personn
    comit : Search works for some options. Marge with Jennifer project

--- models/Class.Personne.php
+++ models/Class.Personne.php
@@ -1,291 +1,294 @@
 <?php //15
 class Personne{
-	private $id;
+  private $id;
 
-	private $firstname;
-	private $lastname;
+  private $firstname;
+  private $lastname;
 
-	private $email;
-	private $password;
 
-	private $role;
+  private $email;
+  private $password;
 
-	private $portable;
-	private $phone;
+  private $role;
 
-	private $adresse;
-	private $localite;
-	private $npa;
 
-	private $idxAbonnement;
+  private $portable;
+  private $phone;
 
-	//It uses null as the default value for the function argument $id
-	public function __construct($id=null, $firstname, $lastname, $adresse, $email, $password=null,
-			$phone, $portable, $numMember,  $estActif, $idxAbonnement,
-			$npa, $localite, $idxLangue, $role){
+  private $adresse;
+  private $localite;
+  private $npa;
 
-				$this->setId($id);
-				$this->setFirstname($firstname);
-				$this->setLastname($lastname);
 
-				$this->setEmail($email);
 
-				$this->setPassword($password);
+  private $idxAbonnement;
 
-				$this->setIdxLangue($idxLangue);
-				$this->setEstActif($estActif);
 
-				$this->setNumMember($numMember);
 
-				$this->setRole($role);
 
-				$this->setPortable($portable);
-				$this->setPhone($phone);
+  //It uses null as the default value for the function argument $id
+  public function __construct($id=null, $firstname, $lastname,
+                              $email, $password=null, $idxLangue, $estActif, $numMember, $role,
+                               $portable, $phone, $adresse, $localite, $npa, $idxAbonnement){
 
-				$this->setAdresse($adresse);
-				$this->setLocalite($localite);
-				$this->setNpa($npa);
+      $this->setId($id);
+      $this->setFirstname($firstname);
+      $this->setLastname($lastname);
 
-				$this->setIdxAbonnement($idxAbonnement);
 
-	}
+      $this->setEmail($email);
 
-	//id
-	public function getId(){
-		return $this->id;
-	}
+      $this->setPassword($password);
 
-	public function setId($id) {
-		$this->id= $id;
-	}
+      $this->setIdxLangue($idxLangue);
+      $this->setEstActif($estActif);
 
-	public function getEmail(){
-		return $this->email;
-	}
+      $this->setNumMember($numMember);
 
-	public function setEmail($email){
-		$this->email = $email;
-	}
+      $this->setRole($role);
 
-	public function getPassword(){
-		return $this->password;
-	}
+      $this->setPortable($portable);
+      $this->setPhone($phone);
+
+      $this->setAdresse($adresse);
+      $this->setLocalite($localite);
+      $this->setNpa($npa);
+
+      $this->setIdxAbonnement($idxAbonnement);
+
+      }
+
+
+  //id
+  public function getId(){
+
+    return $this->id;
+  }
+
+  public function setId($id)
+  {
+    $this->id= $id;
+  }
+
+  public function getEmail(){
+    return $this->email;
+  }
+
+  public function setEmail($email){
+    $this->email = $email;
+  }
+
+  public function getPassword(){
+  		return $this->password;
+  	}
 
 	public function setPassword($password){
-		$this->password = $password;
-	}
+  		$this->password = $password;
+  	}
 
-	//firstname
-	public function getFirstname(){
-		return $this->firstname;
-	}
+//firstname
+  public function getFirstname(){
+    return $this->firstname;
+  }
 
-	public function setFirstname($firstname){
-		$this->firstname = $firstname;
-	}
+  public function setFirstname($firstname){
+    $this->firstname = $firstname;
+  }
 
-	//lastname
-	public function getLastname(){
-		return $this->lastname;
-	}
+  //lastname
+  public function getLastname(){
+    return $this->lastname;
+  }
 
-	public function setLastname($lastname){
-		$this->lastname = $lastname;
-	}
-	 
-	public function getAdresse(){
-		return $this->adresse;
-	}
+  public function setLastname($lastname){
+    $this->lastname = $lastname;
+  }
+ public function getAdresse(){
+   return $this->adresse;
+ }
 
-	public function setAdresse($adresse){
-		$this->adresse = $adresse;
-	}
+ public function setAdresse($adresse){
+   $this->adresse = $adresse;
+ }
 
-	public function getLocalite() {
-		return $this->localite;
-	}
+ public function getLocalite()
+ {
+   return $this->localite;
+ }
 
-	public function setLocalite($localite){
-		$this->localite=$localite;
-	}
-
-
-	public function getNpa() {
-		return $this->npa;
-	}
-
-	public function setNpa($npa){
-		$this->npa=$npa;
-	}
-
-	public function getPortable() {
-		return $this->portable;
-	}
-
-	public function setPortable($portable){
-		$this->portable= $portable;
-	}
-
-	public function getPhone() {
-		return $this->phone;
-	}
-
-	public function setPhone($phone){
-		$this->phone= $phone;
-	}
-
-	public function getNumMember(){
-		return $this->numMember;
-	}
-
-	public function setNumMember($numMember){
-		$this->numMember = $numMember;
-	}
-
-	public function getEstActif(){
-		return $this->estActif;
-	}
-
-	public function setEstActif($estActif){
-		$this->estActif = $estActif;
-	}
-
-	public function getRole(){
-		return $this->role;
-	}
-
-	public function setRole($role){
-		$this->role = $role;
-	}
-
-	public function getIdxAbonnement(){
-		return $this->idxAbonnement;
-	}
-
-	public function setIdxAbonnement($idxAbonnement){
-		$this->idxAbonnement = $idxAbonnement;
-	}
-
-	public function getIdxLangue(){
-		return $this->idxLangue;
-	}
-
-	public function setIdxLangue($idxLangue){
-		$this->idxLangue = $idxLangue;
-	}
-	 
-	public static function connect($adressEmail,$pwd){
-		$pwd = sha1($pwd);
-		$query = "SELECT * From personne WHERE email='$adressEmail' AND motDePasse='$pwd'";
-		$result = MySqlConn::getInstance()->selectDB($query);
-		$row = $result->fetch();
-		if(!$row) return false;
-
-		return new Personne($row['idPersonne'], $row['prenom'], $row['nom'],$row['email'], $row['motDePasse']
-				, $row['idxLangue'], $row['estActif'] , $row['numMembre'], $row['role'], $row['portable'],
-				$row['telephone'], $row['adresse'], $row['localite'], $row['NPA'], $row['idxAbonnement']);
-	}
-
-	public function update($id){
-		$query = "UPDATE personne
-		SET email= '$this->email',
-		portable= '$this->portable',
-		telephone='$this->phone',
-		adresse='$this->adresse',
-		localite='$this->localite',
-		NPA='$this->npa',
-		idxAbonnement='$this->idxAbonnement'
-		WHERE idPersonne='$id'
-		;";
-
-		return  MySqlConn::getInstance()->executeQuery($query);
-	}
-	 
-	public function update2($id){
-		$query = "UPDATE personne
-		SET lastname= '$this->lastname',
-		firstname= '$this->firstname',
-		adresse='$this->adresse',
-		npa='$this->npa',
-		localite='$this->localite',
-		portable='$this->portable',
-		phone='$this->phone',
-		email='$this->email'
-
-		WHERE idPersonne='$id'
-		;";
-		 
-		return  MySqlConn::getInstance()->executeQuery($query);
-	}
-
-	 
-	public static function connectToAll() {
-		$query =  "SELECT * From personne ";
-		$result = MySqlConn::getInstance()->selectDB($query);
-		while($row = $result->fetch()) {
-			
-			$resultArray[$row['idPersonne']] =   new Personne($row['idPersonne'], $row['prenom'], $row['nom'], $row['adresse'], $row['email'], $row['motDePasse']
-					, $row['telephone'], $row['portable'], $row['idxLangue'], $row['estActif'] , $row['numMembre'], $row['Localite'], $row['NPA'], $row['role'],
-					$row['idxAbonnement']);
-				
-		}
-
-		$result->closeCursor();
-		return $resultArray;
-	}
-
-	public static function checkExist($adressEmail,$name,$lastname){
-		$query = "SELECT * From personne WHERE email='$adressEmail' AND nom='$lastname' AND prenom='$name'";
-		$result = MySqlConn::getInstance()->selectDB($query);
-		$row = $result->fetch();
-		if(!$row) return false;
-
-		return new Personne($row['idPersonne'], $row['prenom'], $row['nom'],$row['email'], $row['motDePasse']
-				, $row['idxLangue'], $row['estActif'] , $row['numMembre'], $row['role'], $row['portable'],
-				$row['telephone'], $row['adresse'], $row['Localite'], $row['NPA'], $row['idxAbonnement']);
-	}
+ public function setLocalite($localite){
+   $this->localite=$localite;
+ }
 
 
-	public static function CreateNonCAS($user){
-		$lastnameUser = $user->getLastname();
-		$firstnameUser = $user->getFirstname();
-		$adresseUser =   $user->getAdresse();
-		$emailUser = $user->getEmail();
-		$phoneUser =  $user->getPhone();
-		$portableUser = $user->getPortable();
-		if($user->getIdxAbonnement()==null) {
-			$idxAbonnementUser=0;
-		} else {
-			$idxAbonnementUser = $user->getIdxAbonnement();
-		}
+ public function getNpa()
+ {
+   return $this->npa;
+ }
 
-		$npaUser = $user->getNpa();
-		$localityUser = $user->getLocalite();
+  public function setNpa($npa){
+    $this->npa=$npa;
+  }
 
-		$query = "INSERT INTO `personne`(`nom`, `prenom`, `adresse`, `email`, `telephone`, `portable`,`idxAbonnement`, `NPA`, `localite`)
-		VALUES ('$lastnameUser','$firstnameUser','$adresseUser','$emailUser','$phoneUser','$portableUser','$idxAbonnementUser','$npaUser','$localityUser');";
+  public function getPortable()
+  {
+    return $this->portable;
+  }
 
-		MySqlConn::getInstance()->executeQuery($query);
-		//setup l'id de ce USER
+   public function setPortable($portable){
+     $this->portable= $portable;
+   }
 
-		$idquery = "SELECT idPersonne From personne
-		WHERE nom     ='$lastnameUser'
-		AND prenom    ='$firstnameUser'
-		AND adresse   ='$adresseUser'
-		AND email     ='$emailUser'
-		AND NPA       ='$npaUser'
-		AND Localite  ='$localityUser'
-		";
+   public function getPhone()
+   {
+     return $this->phone;
+   }
+
+    public function setPhone($phone){
+      $this->phone= $phone;
+    }
+
+    public function getNumMember(){
+  		return $this->numMember;
+  	}
+
+  	public function setNumMember($numMember){
+  		$this->numMember = $numMember;
+  	}
+
+    public function getEstActif(){
+      return $this->estActif;
+    }
+
+    public function setEstActif($estActif){
+      $this->estActif = $estActif;
+    }
+
+    public function getRole(){
+      return $this->role;
+    }
+
+    public function setRole($role){
+      $this->role = $role;
+    }
+
+    public function getIdxAbonnement(){
+      return $this->idxAbonnement;
+    }
+
+    public function setIdxAbonnement($idxAbonnement){
+      $this->idxAbonnement = $idxAbonnement;
+    }
+
+    public function getIdxLangue(){
+      return $this->idxLangue;
+    }
+
+    public function setIdxLangue($idxLangue){
+      $this->idxLangue = $idxLangue;
+    }
+public static function connect($adressEmail,$pwd){
+
+  $pwd = sha1($pwd);
+  $query = "SELECT * From personne WHERE email='$adressEmail' AND motDePasse='$pwd'";
+  $result = MySqlConn::getInstance()->selectDB($query);
+  $row = $result->fetch();
+  if(!$row) return false;
+
+  return new Personne($row['idPersonne'], $row['prenom'], $row['nom'],$row['email'], $row['motDePasse']
+                      , $row['idxLangue'], $row['estActif'] , $row['numMembre'], $row['role'], $row['portable'],
+                       $row['telephone'], $row['adresse'], $row['localite'], $row['NPA'], $row['idxAbonnement']);
 
 
-		$result = MySqlConn::getInstance()->selectDB($idquery);
-		$row = $result->fetch();
-		if(!$row) return false;
-
-		$user->setId($row['idPersonne']);
-		$user->getId();
-	}
-	 
 }
 
-?>
+public function update($id){
+
+  $query = "UPDATE personne
+            SET email= '$this->email',
+                portable= '$this->portable',
+                telephone='$this->phone',
+                adresse='$this->adresse',
+                localite='$this->localite',
+                NPA='$this->npa',
+                idxAbonnement='$this->idxAbonnement'
+            WHERE idPersonne='$id'
+            ;";
+
+  return  MySqlConn::getInstance()->executeQuery($query);
+
+}
+
+
+public static function checkExist($adressEmail,$name,$lastname){
+
+
+  $query = "SELECT * From personne WHERE email='$adressEmail' AND nom='$lastname' AND prenom='$name'";
+  $result = MySqlConn::getInstance()->selectDB($query);
+  $row = $result->fetch();
+  if(!$row) return false;
+
+  return new Personne($row['idPersonne'], $row['prenom'], $row['nom'],$row['email'], $row['motDePasse']
+                      , $row['idxLangue'], $row['estActif'] , $row['numMembre'], $row['role'], $row['portable'],
+                       $row['telephone'], $row['adresse'], $row['Localite'], $row['NPA'], $row['idxAbonnement']);
+
+
+}
+
+
+public static function CreateNonCAS($user){
+
+
+  $lastnameUser = $user->getLastname();
+  $firstnameUser = $user->getFirstname();
+  $adresseUser =   $user->getAdresse();
+  $emailUser = $user->getEmail();
+  $phoneUser =  $user->getPhone();
+  $portableUser = $user->getPortable();
+  if($user->getIdxAbonnement()==null)
+  {
+
+    $idxAbonnementUser=0;
+  }else {
+      $idxAbonnementUser = $user->getIdxAbonnement();
+  }
+
+  $npaUser = $user->getNpa();
+  $localityUser = $user->getLocalite();
+
+
+  $query = "INSERT INTO `personne`(`nom`, `prenom`, `adresse`, `email`, `telephone`, `portable`,`idxAbonnement`, `NPA`, `localite`)
+          VALUES ('$lastnameUser','$firstnameUser','$adresseUser','$emailUser','$phoneUser','$portableUser','$idxAbonnementUser','$npaUser','$localityUser');";
+
+   MySqlConn::getInstance()->executeQuery($query);
+//setup l'id de ce USER
+
+  $idquery = "SELECT idPersonne From personne
+                WHERE nom     ='$lastnameUser'
+                AND prenom    ='$firstnameUser'
+                AND adresse   ='$adresseUser'
+                AND email     ='$emailUser'
+                AND NPA       ='$npaUser'
+                AND Localite  ='$localityUser'
+                  ";
+
+
+      $result = MySqlConn::getInstance()->selectDB($idquery);
+      $row = $result->fetch();
+      if(!$row) return false;
+
+      $user->setId($row['idPersonne']);
+      return getId();
+;
+
+}
+
+
+
+
+
+
+
+}
+
+ ?>
